70cbf9e8cd895f685fb5b45a77126946b33890f4
qiziyun123 <qiziyun123@sina.com>
2019-03-14 16:43:18 +0800

    123

---
+++ javaleetcode/src/main/java/qizy/algorithm/sort/MergeSort.java
@@ -0,0 +1,8 @@
+package qizy.algorithm.sort;
+
+public class MergeSort {
+	public static void main(String[] args) {
+		int[] a = {5,3,4,8,2,7,1,9,11,6,5};
+
+	}
+}
---
+++ javaleetcode/src/main/java/qizy/leetcode/easy/onehundred/TwoNums.java
@@ -0,0 +1,10 @@
+package qizy.leetcode.easy.onehundred;
+
+public class TwoNums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
