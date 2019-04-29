a2397d59aabc16d63e9a960c5ce388bd2ba513fe
qiziyun123 <qiziyun123@sina.com>
2019-04-29 10:34:52 +0800

    dfs depth of tree

--- javaleetcode/src/main/java/qizy/leetcode/dfs/medium/MaximumDepthofNaryTree.java
+++ javaleetcode/src/main/java/qizy/leetcode/dfs/medium/MaximumDepthofNaryTree.java
@@ -1,11 +1,41 @@
 package qizy.leetcode.dfs.medium;
 
+import java.util.List;
+
 public class MaximumDepthofNaryTree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
-	
+	class Node {
+		public int val;
+		public List<Node> children;
+
+		public Node() {
+		}
+
+		public Node(int _val, List<Node> _children) {
+			val = _val;
+			children = _children;
+		}
+	}
+
+	public int maxDepth(Node root) {
+		if(root==null) {
+			return 0;
+			
+		}else if(root.children==null||root.children.size()==0){
+			return 1;
+		}else {
+			List<Node> children = root.children;
+			int max=0;
+			for (Node node : children) {
+				max = Math.max(maxDepth(node), max);
+			}
+			return max+1;
+		}
+		
+	}
+
 }
